c734296c351bdc5bdf952d2782fa4e854554d046
lxcao <caolingxin@hotmail.com>
2021-04-05 20:02:14 +0800

    chapter4

---
+++ Chap4/try-react/src/Home.tsx
@@ -0,0 +1,13 @@
+/*
+ * @Author: your name
+ * @Date: 2021-04-05 14:43:11
+ * @LastEditTime: 2021-04-05 14:45:08
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /HandsOnTypeScript/Chap4/try-react/src/Home.tsx
+ */
+import React, { FC } from "react";
+const Home: FC = () => {
+    return <div>Hello World! Home</div>;
+};
+export default Home;
